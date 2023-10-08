da47295ae0592d5cf33db95819cb23141f20dca9
Jangjun <daki403.1@gmail.com>
2023-10-08 18:09:07 +0900

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:time_traveler/page/home.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Time_Traveler',
+      theme: ThemeData(
+          primaryColor: const Color.fromRGBO(53, 162, 159, 1.0),
+          brightness: Brightness.dark,
+          fontFamily: GoogleFonts.notoSansKr().fontFamily,
+          textTheme: const TextTheme(
+            displayLarge: TextStyle(
+              fontSize: 72.0,
+              fontWeight: FontWeight.bold,
+            ),
+            displayMedium: TextStyle(
+              fontSize: 36.0,
+            ),
+            displaySmall: TextStyle(
+              fontSize: 14.0,
+              color: Color.fromRGBO(245, 245, 220, 1),
+            ),
+          )),
+      home: const Home(),
+    ),
+  );
+}
