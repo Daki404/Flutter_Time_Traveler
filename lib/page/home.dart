1bff8a54d0e3f4488d04f2540c1b3fe0367e8e70
Jangjun <daki403.1@gmail.com>
2023-10-12 09:47:47 +0900

    Feat: mic icon 및 neon_animation 추가
    neon_widgets 라이브러리를 사용하여
    neon_circle의 mic icon과 stack으로 표현 하였습니다
    
    Related to: #1

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:neon_widgets/neon_widgets.dart';
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -11,9 +13,31 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Text(
-      'Home',
-      style: Theme.of(context).textTheme.displayLarge,
-    ));
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              'Home',
+              style: Theme.of(context).textTheme.displaySmall,
+            ),
+            Stack(
+              children: [
+                Container(
+                    padding: EdgeInsets.fromLTRB(4, 60, 0, 0),
+                    alignment: Alignment.center,
+                    child: FlickerNeonPoint(
+                      spreadColor: Theme.of(context).primaryColor,
+                      flickerTimeInMilliSeconds: 2000,
+                      lightSpreadRadius: 40,
+                      lightBlurRadius: 100,
+                      randomFlicker: false,
+                    )),
+                Center(child: Image.asset('assets/mic.png')),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
